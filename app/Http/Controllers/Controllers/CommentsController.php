f65cf160b322e0cd043ac5e3a89b1ad786e098f0
noorhayder-ev7 <59250587+noorhayder-ev7@users.noreply.github.com>
2020-02-20 11:22:59 +0300

    Add files via upload

---
+++ app/Http/Controllers/Controllers/CommentsController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Controllers;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CommentsController extends Controller
+{
+    public function getcomment(Request $request)
+    {
+        $cmt = new \App\models\Comments;
+        $cmt->user_id=$request->input('user_id');
+        $cmt->post_id=$request->input('post_id');
+        $cmt->content = $request->input('content');
+        $mydate = Carbon::now();
+        $mydate->toDateTimeString();
+        $cmt->created_at = $mydate;
+        $cmt->save();
+        if ($cmt)
+        {
+            return response()->json($cmt);
+
+        }
+        else
+            return response()->json(['message'=> 'NOT FOUND']);
+
+    }
+    public function deletecomment(Request $request){
+
+        $body = $request->all();
+        $id= $body['id'];
+        $data = (new \App\models\Comments)->where('id', $id)->delete();
+        if ($data)
+        {
+            return response()->json(['message'=> 'DONE']);
+
+        }
+        else
+            return response()->json(['message'=> 'NOT FOUND']);
+
+
+    }
+}
--- app/Http/Controllers/Controllers/Postcontroller.php
+++ app/Http/Controllers/Controllers/Postcontroller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -13,4 +14,44 @@ class Postcontroller extends Controller
         $data= (new \App\models\Post)->paginate(10);
         return response()->json($data);
     }
+    public function getposts(Request $request)
+    {
+        $post = new \App\models\Post;
+        $post->user_id=$request->input('user_id');
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $mydate = Carbon::now();
+        $mydate->toDateTimeString();
+        $post->created_at = $mydate;
+        $post->rate="0";
+        $post->views="0";
+        $post->tags="NULL";
+        $post->status="0";
+        $post->category_id=$request->input('category_id');
+        $post->save();
+        if ($post)
+        {
+            return response()->json($post);
+
+        }
+        else
+            return response()->json(['message'=> 'NOT FOUND']);
+
+    }
+    public function deletepost(Request $request){
+
+        $body = $request->all();
+        $id= $body['id'];
+        $data = (new \App\models\Post)->where('id', $id)->delete();
+        if ($data)
+        {
+            return response()->json(['message'=> 'DONE']);
+
+        }
+        else
+            return response()->json(['message'=> 'NOT FOUND']);
+
+
+    }
 }
+
