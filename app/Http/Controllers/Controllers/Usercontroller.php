93fbf2836955d6e4c1a553f755e16fb38cb1eea3
noorhayder-ev7 <59250587+noorhayder-ev7@users.noreply.github.com>
2020-04-03 01:32:12 +0300

    Add files via upload

--- app/Http/Controllers/Controllers/Usercontroller.php
+++ app/Http/Controllers/Controllers/Usercontroller.php
@@ -68,7 +68,10 @@ class Usercontroller extends Controller
 
         if ($userFromDB1) {
             $data = User::where('email', $username)->where('password', md5($password))->first();
-            return response()->json($data);
+            if($data)
+              return response()->json($data);
+            else
+                return response()->json(['message'=> 'NOT FOUND']);
         }
 
         elseif ($userFromDB2)
